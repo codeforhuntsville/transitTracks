a03f75c9836c467f31b1462b3b880593c8c01ea6
G Larry Mason <lmason@HungryProgrammer.com>
2015-11-12 23:16:05 -0600

    more tests

--- public/js/hsvttload.js
+++ public/js/hsvttload.js
@@ -1,10 +1,18 @@
 HSV_TT = {};
 
+
 var map = L.map('transitMap').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+
+$(document).ready(function(){
+	
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'pk.eyJ1IjoiaHN2dHJhbnNpdCIsImEiOiJjaWd4NXR5bDcwdWdiNjVtMHJqajByZ2FwIn0.MGnCx-SYksm4Ia8-4CoWMg',
     accessToken: 'pk.eyJ1IjoiaHN2dHJhbnNpdCIsImEiOiJjaWd4NXR5bDcwdWdiNjVtMHJqajByZ2FwIn0.MGnCx-SYksm4Ia8-4CoWMg'
-}).addTo(map);
+  }).addTo(map);
+
+  // read text entries from xml file
+}
+ 
