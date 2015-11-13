673dd6d1b59f061626e1faf400ecfe14cadf3f78
G Larry Mason <lmason@HungryProgrammer.com>
2015-11-13 00:48:55 -0600

    wow

--- public/js/hsvttload.js
+++ public/js/hsvttload.js
@@ -2,15 +2,15 @@ HSV_TT = {};
 
 var map = L.map('transitMap').setView([34.71, -86.74], 4);
 
+
+$(document).ready(function(){
+	
+ 
  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'hsvtransit.cigx5tx9c0u474mm3jqvacywa',
     accessToken: 'pk.eyJ1IjoiaHN2dHJhbnNpdCIsImEiOiJjaWd4NXR5bDcwdWdiNjVtMHJqajByZ2FwIn0.MGnCx-SYksm4Ia8-4CoWMg'
   }).addTo(map);
-
-$(document).ready(function(){
-	
- 
   
 }
