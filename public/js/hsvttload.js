9b8ba862ea17b23677253c691cdaab6b940aad18
G Larry Mason <lmason@HungryProgrammer.com>
2015-11-13 00:14:05 -0600

    test

--- public/js/hsvttload.js
+++ public/js/hsvttload.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
-    id: 'svtransit.cigx5tx9c0u474mm3jqvacywa',
+    id: 'hsvtransit.cigx5tx9c0u474mm3jqvacywa',
     accessToken: 'pk.eyJ1IjoiaHN2dHJhbnNpdCIsImEiOiJjaWd4NXR5bDcwdWdiNjVtMHJqajByZ2FwIn0.MGnCx-SYksm4Ia8-4CoWMg'
   }).addTo(map);
   
