e265cdb431de17b88482485c6ee31738ba522e4d
G Larry Mason <lmason@HungryProgrammer.com>
2015-11-12 22:48:46 -0600

    test

--- public/js/hsvttload.js
+++ public/js/hsvttload.js
@@ -2,7 +2,7 @@ HSV_TT = {};
 
 var map = L.map('transitMap').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/transitMap/13/51.505/-0.09.png?access_token={pk.eyJ1IjoiaHN2dHJhbnNpdCIsImEiOiJjaWd4NXR5bDcwdWdiNjVtMHJqajByZ2FwIn0.MGnCx-SYksm4Ia8-4CoWMg}', {
+L.tileLayer('https://api.tiles.mapbox.com/v4/hsvtransit/13/51.505/-0.09.png?access_token={pk.eyJ1IjoiaHN2dHJhbnNpdCIsImEiOiJjaWd4NXR5bDcwdWdiNjVtMHJqajByZ2FwIn0.MGnCx-SYksm4Ia8-4CoWMg}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'pk.eyJ1IjoiaHN2dHJhbnNpdCIsImEiOiJjaWd4NXR5bDcwdWdiNjVtMHJqajByZ2FwIn0.MGnCx-SYksm4Ia8-4CoWMg',
