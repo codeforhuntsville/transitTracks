e24638df0b9321d984d5dc701cb8047d2d8bbcd5
Noah Huber-Feely <nhuberfeely@gmail.com>
2015-11-15 16:14:17 -0600

    Bug fix testing

--- hsvtt.js
+++ hsvtt.js
@@ -100,7 +100,7 @@ var latLng = [34.73172, -86.58979];
 //Everything socket.io related
 io.sockets.on('get location', function(data) {
   console.log('location update requested');
-  io.emit('location update', latLng);
+  io.send('location update', latLng);
   console.log('location update sent');
   latLng[0] += 0.5;
   latLng[1] += 0.5;
