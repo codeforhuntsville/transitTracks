094af35ac636d6715f2da6db474d81437b85c300
Noah Huber-Feely <nhuberfeely@gmail.com>
2015-11-15 13:50:53 -0600

    Adding in socket support

--- public/js/hsvttsockets.js
+++ public/js/hsvttsockets.js
@@ -1,1 +1,21 @@
 HSV_TT.sockets = {};
+
+HSV_TT.sockets.init = function() {
+  var socket = io.connect();
+  var location = {'lng': 34.731, 'lat': -86.586};
+  function updateMap() {
+    console.log(location);
+  };
+  function receiveUpdates() {
+    socket.on('location update', function(data) {
+      location['lng'] = data[0];
+      location['lat'] = data[1];
+      updateMap();
+    }
+  }
+  receiveUpdates();
+  function updateLocation() {
+    socket.emit('get location');
+  };
+  setInterval(updateLocation(), 3000);
+};
