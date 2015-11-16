05e65792b24fe49ee996dd4c0acf503a306072dd
Noah Huber-Feely <nhuberfeely@gmail.com>
2015-11-16 15:34:28 -0600

    Fix typo in variable name

--- hsvtt.js
+++ hsvtt.js
@@ -106,8 +106,8 @@ function findLocations() {
 	console.log('Updating current location');
 	Transit.find({pass: process.env.PASS}, function(err, transit) {
 		if( transit[0] ) {
-			latLng[0] = transit[0].lat;
-			latLng[1] = transit[0].long;
+			latLng[0] = 34.73172;//transit[0].lat;
+			latLng[1] = -86.58979;transit[0].long;
 		} else {
 			console.log('DB credentials supplied incorrect');
 		}
