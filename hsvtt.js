5e48675e7ee148949770c7826935c53d13cde7c5
G Larry Mason <lmason@HungryProgrammer.com>
2015-11-28 16:12:08 -0600

    commented out some log statements

--- hsvtt.js
+++ hsvtt.js
@@ -68,7 +68,7 @@ app.get('/api/v1/account/:id', function(req, res) {
 app.post('/api/v1/trolly/:id/location', function(req, res) {
 	var transitId = req.params.id;
 	Transit.find({id: transitId}, function( err, transit ) {
-		console.log('Finding if transit exists: id = ' + transitId + ": " + transit[0]);
+		//console.log('Finding if transit exists: id = ' + transitId + ": " + transit[0]);
 		if( transit[0] ) {
 			Transit.find({id: transitId}, function( err, transit ) {
 				if( transit[0] ) {
@@ -85,7 +85,7 @@ app.post('/api/v1/trolly/:id/location', function(req, res) {
 		} else {
 			newTransit = new Transit( {id: transitId, long: req.body.lon, lat: req.body.lat, pass: "ok" } ); 
 			newTransit.save();
-			console.log('New bus added');
+			//console.log('New bus added');
 		}
 	});
 	res.send('Location added');
@@ -113,9 +113,9 @@ var latLongs = {};
 var homeLatLng = [34.73689, -86.592192];
 
 function findLocations() {
-	console.log('Updating current location');
+	//console.log('Updating current location');
 	Transit.find({pass: "ok"},{id:1,lat:1,long:1,_id:0}, function(err, transit) {
-		console.log("Getting coords for " + transit.length)
+		//console.log("Getting coords for " + transit.length)
 		if( transit.length > 0 ) {
 			allLocations = transit;
 		} else {
@@ -129,7 +129,7 @@ var interval = setInterval(function(){findLocations();},3000);
 //Everything socket.io related
 io.sockets.on('connection', function(socket) {
 	socket.on('get location', function( data ) {
-		console.log('location update requested ');
+		//console.log('location update requested ');
 		io.emit('location update', allLocations);
 	});
     socket.on('disconnect', function() {
