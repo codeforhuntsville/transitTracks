8defe549e35d2a87f80a8665f73aa35859cb38d3
G Larry Mason <lmason@HungryProgrammer.com>
2015-11-28 13:16:56 -0600

    working

--- public/js/hsvttsockets.js
+++ public/js/hsvttsockets.js
@@ -12,8 +12,8 @@ HSV_TT.sockets.init = function() {
     console.log('Initializing location updates');
     socket.on('location update', function(data) {
       console.log('=== New location received: ' + JSON.stringify(data));	  
-      //location.lat = data[0];
-      //location.lng = data[1];
+      location.lat = data[0];
+      location.lng = data[1];
       updateMap();
     });
   }
