29edc03c9a74589e5ce7a34e1fabd5baf8410162
Noah Huber-Feely <nhuberfeely@gmail.com>
2015-11-29 15:57:50 -0600

    Remove unnecesary db var

--- hsvtt.js
+++ hsvtt.js
@@ -14,7 +14,6 @@ var transitSchema = new mongoose.Schema({
 	id: Number,
 	long: Number,
 	lat: Number,
-	pass: String
 });
 var Transit = mongoose.model('Transit', transitSchema);
 var allLocations = [];
@@ -47,7 +46,7 @@ app.post('/api/v1/account', function(req, res) {
 		if( transit[0] ) {
 			res.send('Account already created');
 		} else {
-			newTransit = new Transit( {id: transitId, lat: 0, long: 0, pass: process.env.PASS} ); 
+			newTransit = new Transit( {id: transitId, lat: 0, long: 0} ); 
 			newTransit.save();
 			res.send('Account created');
 		}
@@ -83,7 +82,7 @@ app.post('/api/v1/trolly/:id/location', function(req, res) {
 				}
 			});
 		} else {
-			newTransit = new Transit( {id: transitId, long: req.body.lon, lat: req.body.lat, pass: "ok" } ); 
+			newTransit = new Transit( {id: transitId, long: req.body.lon, lat: req.body.lat} ); 
 			newTransit.save();
 			//console.log('New bus added');
 		}
@@ -114,7 +113,7 @@ var homeLatLng = [34.73689, -86.592192];
 
 function findLocations() {
 	//console.log('Updating current location');
-	Transit.find({pass: "ok"},{id:1,lat:1,long:1,_id:0}, function(err, transit) {
+	Transit.find({},{id:1,lat:1,long:1,_id:0}, function(err, transit) {
 		//console.log("Getting coords for " + transit.length)
 		if( transit.length > 0 ) {
 			allLocations = transit;
