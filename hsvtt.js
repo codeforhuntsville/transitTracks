68c77c6c9ad9c385a8d24f99e044b37d79a359e6
G Larry Mason <lmason@HungryProgrammer.com>
2016-03-06 18:00:05 -0600

    sunday testing...

--- hsvtt.js
+++ hsvtt.js
@@ -111,13 +111,15 @@ app.get('/api/v1/account/:id', function(req, res) {
 app.post('/api/v1/trolly/:id/location', function(req, res) {
 	var returnStr = "record added";
 	var transitId = req.params.id;
+	console.log('attempt 1 (transitId) = ' + transitId);
 	Transit.find({id: transitId}, function( err, transit ) {
 		console.log('location for vehicle = ' + transitId);
 		if( transit[0] ) {
 			Transit.find({id: transitId}, function( err, transit ) {
 				if( transit[0] ) {
 					//console.log('Recording location to DB: ' + transit[0].id);
-					transit[0].id = req.params.id;		
+					transit[0].id = req.params.id;
+                    console.log('attempt 2 (lat & lng) = ' + req.body.lat + ":" +req.body.lat);					
 					if (geoUtils.contains([req.body.lat,req.body.lng], geoConst.dtBounds)) {
 						transit[0].lat = req.body.lat;
 					    transit[0].long = req.body.lng;
