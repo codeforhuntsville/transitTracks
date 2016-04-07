33b7c3a6017abcad09498740e81b2e41aa19ac26
G Larry Mason <lmason@HungryProgrammer.com>
2016-04-07 00:52:15 -0500

    typo

--- hsvtt.js
+++ hsvtt.js
@@ -283,7 +283,7 @@ function locationRecieved(data) {
   var returnStr = "location socket called: ";
   var transitId = data.id;
   var vehicleFound = false;
-  console.log("here: " data.id + " : " + data.lat + " - " + data.lon );
+  console.log("here: " + data.id + " : " + data.lat + " - " + data.lon );
   for(var i = 0; i < vehicles.length; i++) {
     if(vehicles[i]['id']==transitId) {
       vehicleFound = true;
